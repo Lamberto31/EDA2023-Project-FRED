3d789804d28fa7829f1faa29da4162cc16ad8fd4
Lamberto Preite <78571193+Lamberto31@users.noreply.github.com>
2023-07-14 23:30:55 +0200

    Definiti pin digitali

--- EDA2023-Project-Willy.ino
+++ EDA2023-Project-Willy.ino
@@ -1,3 +1,16 @@
+#define PIN_ESP_TX 2
+#define PIN_ESP_RX 3
+#define PIN_ULTRASONIC_ECHO 4
+#define PIN_ULTRASONIC_TRIG 5
+#define PIN_SERVO_HORIZ 6
+#define PIN_MOTOR_IN4 7
+#define PIN_MOTOR_IN3 8
+#define PIN_MOTOR_ENB 9
+#define PIN_IR_RECV 10
+#define PIN_MOTOR_ENA 11
+#define PIN_MOTOR_IN2 12
+#define PIN_MOTOR_IN1 13
+
 void setup() {
   // put your setup code here, to run once:
 
