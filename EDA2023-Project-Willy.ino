eea69211097261f86a28643ae3f8521b61426b86
Lamberto Preite <78571193+Lamberto31@users.noreply.github.com>
2023-07-15 00:55:21 +0200

    Aggiunta funzione per misurare distanza

--- EDA2023-Project-Willy.ino
+++ EDA2023-Project-Willy.ino
@@ -18,10 +18,10 @@
 #define STATE_SETUP 0
 #define STATE_FREE 1
 #define STATE_SEARCH 2
-#define STATE_MEASURE 3 
+#define STATE_MEASURE 3
 byte state = STATE_SETUP;
 
-//IR Button-Command
+// IR Button-Command
 #define IR_BUTTON_1 0x45
 #define IR_BUTTON_2 0x46
 #define IR_BUTTON_3 0x47
@@ -40,11 +40,17 @@ byte state = STATE_SETUP;
 #define IR_BUTTON_LEFT 0x8
 #define IR_BUTTON_OK 0x1C
 
-
+// Ultrasonic
+#define DECIMALS 4
 void setup() {
   Serial.begin(9600);
-  
+
+  // IR Receiver
   IrReceiver.begin(PIN_IR_RECV, 0);
+
+  // Ultrasonic
+  pinMode(PIN_ULTRASONIC_TRIG, OUTPUT);
+  pinMode(PIN_ULTRASONIC_ECHO, INPUT);
 
 }
 
@@ -55,6 +61,7 @@ void loop() {
     switch (IrReceiver.decodedIRData.command) {
       case IR_BUTTON_OK: {
         Serial.println("OK");
+        Serial.println(measureDistance(), DECIMALS);
         break;
       }
       case IR_BUTTON_UP: {
@@ -78,4 +85,20 @@ void loop() {
       }
     }
   }
+}
+
+double measureDistance(){
+  long tripTime;
+  double distance;
+
+  digitalWrite(PIN_ULTRASONIC_TRIG, LOW);
+  delayMicroseconds(5);
+  digitalWrite(PIN_ULTRASONIC_TRIG, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(PIN_ULTRASONIC_TRIG, LOW);
+
+  tripTime = pulseIn(PIN_ULTRASONIC_ECHO, HIGH);
+  distance = 0.0343 * tripTime / 2.0;
+
+  return distance;
 }
