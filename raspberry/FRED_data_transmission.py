82bd9657af6fcea1f40ba6a6659a1db0c16887e7
Lamberto Preite <78571193+Lamberto31@users.noreply.github.com>
2023-09-25 18:03:30 +0200

    Implementata ricezione seriale e controllo BDT

--- raspberry/FRED_data_transmission.py
+++ raspberry/FRED_data_transmission.py
@@ -12,3 +12,19 @@ ser = serial.Serial(
 
 # Check if the serial connection is open
 ser.isOpen()
+
+# MAIN LOOP: receive data from Bluetooth and send to remote server via WiFi
+while True:
+    # RECEIVE DATA FROM BLUETOOTH
+    # Check if there are incoming data
+    if ser.inWaiting() > 0:
+        # Read data
+        recv = ser.readline()
+        # If contains "START" it's a BDT messagge
+        if "START" in recv:
+            print(str(recv, 'utf-8'))
+            while True:
+                recv = ser.readline()
+                print(str(recv, 'utf-8'))
+                if "END" in recv:
+                    break
