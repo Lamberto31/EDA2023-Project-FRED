8a91e0f6a06f7428125df359513bca01d592cc46
Lamberto Preite <78571193+Lamberto31@users.noreply.github.com>
2023-10-01 19:27:15 +0200

    Aggiunto sudo per evitare problemi con librerie python

--- raspberry/FRED.sh
+++ raspberry/FRED.sh
@@ -5,7 +5,7 @@ mac="00:22:04:01:17:C7"
 # Check if rfcomm connection is defined for HC-05 mac address
 if ! rfcomm | grep -q $mac; then
     echo "Connection not defined, executing rfcomm..."
-    rfcomm bind 1 $mac
+    sudo rfcomm bind 1 $mac
     # Check again to see if command has been correctly executed
     if ! rfcomm | grep -q $mac; then
         echo "Connection not defined, try again with sudo permission, exiting..."
