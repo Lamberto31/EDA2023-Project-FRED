1bed5681cd1759c0cd65309bfc9f93452c7956e1
Lamberto Preite <78571193+Lamberto31@users.noreply.github.com>
2023-10-01 19:24:01 +0200

    Aggiunto controllo corretta esecuzione rfcomm

--- raspberry/FRED.sh
+++ raspberry/FRED.sh
@@ -6,6 +6,11 @@ mac="00:22:04:01:17:C7"
 if ! rfcomm | grep -q $mac; then
     echo "Connection not defined, executing rfcomm..."
     rfcomm bind 1 $mac
+    # Check again to see if command has been correctly executed
+    if ! rfcomm | grep -q $mac; then
+        echo "Connection not defined, try again with sudo permission, exiting..."
+        exit 1
+    fi
 fi
 echo "Executing python script..."
 python3 FRED_data_transmission.py
