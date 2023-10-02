01484e9c928d4bb3b6deffae59d7cad98373a5f5
Lamberto Preite <78571193+Lamberto31@users.noreply.github.com>
2023-10-02 17:37:15 +0200

    Fix problema di esecuzione FRED.sh se sudo

--- raspberry/FRED.sh
+++ raspberry/FRED.sh
@@ -5,12 +5,23 @@ mac="00:22:04:01:17:C7"
 # Check if rfcomm connection is defined for HC-05 mac address
 if ! rfcomm | grep -q $mac; then
     echo "Connection not defined, executing rfcomm..."
+    # Check if script is executed with sudo permission
+    if [ "$EUID" -ne 0 ]; then
+        echo "Please run as root to execute rfcomm bind, exiting..."
+        exit 1
+    fi
     sudo rfcomm bind 1 $mac
     # Check again to see if command has been correctly executed
     if ! rfcomm | grep -q $mac; then
         echo "Connection not defined, try again with sudo permission, exiting..."
         exit 1
     fi
-fi
+
+# Check if script is executed with sudo permission
+    if [ "$EUID" -eq 0 ]; then
+        echo "Please run as pi to execute python script, exiting..."
+        exit 1
+    fi
+
 echo "Executing python script..."
 python3 FRED_data_transmission.py
