e567fae0e3a45c945d1e91eeb12bb433b0c1fdfe
Lamberto Preite <78571193+Lamberto31@users.noreply.github.com>
2023-10-06 15:44:59 +0200

    Implementato passaggio argomenti a script bash

--- raspberry/FRED.sh
+++ raspberry/FRED.sh
@@ -1,18 +1,58 @@
 #!/bin/bash
 
+# USAGE AND ARGUMENTS
 # Usage and help definition
-usage="$(basename "$0") [-h] -- program that check if rfcomm connection is defined and then execute python script.\n
+usage="$(basename "$0") [-h] [-d {0,1,2}] [-w {0,1}] -- program that check if rfcomm connection is defined and then execute python script.\n
     \tIf a connection is not defined, you need to execute this script with sudo permission and then execute it again without sudo permission.\n
     \tIf a connection is already defined you can execute this script without sudo permission.\n
     \tOptions:\n
-        \t\t-h  show this help text"
+        \t\t-h, --help:\t show this help text\n
+        \t\t-d, --debug:\t enable debug mode for python script, 0 for none, 1 for default, 2 for full\n
+        \t\t-w, --wifi:\t enable wifi mode for python script, 0 for disable, 2 for enable"
+
+# Default arguments
+HELP=0
+DEBUG=1
+WIFI=1
+
+# Parse arguments
+POSITIONAL_ARGS=()
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -h|--help)
+      HELP=true
+      shift # past argument
+      ;;
+    -d|--debug)
+      DEBUG="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -w|--wifi)
+      WIFI="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
+set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
 
 # Check if option -h is passed
-if [ "$1" == "-h" ]; then
+if [ "$HELP" == "true" ]; then
   echo -e $usage
   exit 0
 fi
 
+# MAIN PROGRAMS
 # Mac of HC-05 bluetooth module
 mac="00:22:04:01:17:C7"
 
@@ -42,4 +82,4 @@ fi
     fi
 
 echo "Executing python script..."
-python3 FRED_data_transmission.py
+python3 FRED_data_transmission.py -d $DEBUG -w $WIFI
