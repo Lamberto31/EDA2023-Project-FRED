2d68645592bdbc85c07db2c3f627d18cb1073dc7
Lamberto Preite <78571193+Lamberto31@users.noreply.github.com>
2023-10-17 16:32:57 +0200

    Modificato view_data.m per utilizzo tabella e non matrix per aggiungere objective

--- matlab/view_data.m
+++ matlab/view_data.m
@@ -3,7 +3,7 @@
 logsPath = '../raspberry/logs/';
 
 % Name of csv file with extension
-csvName = 'FRED_log_2023-10-06T17_39_04.csv';
+csvName = 'FRED_log_2023-10-17T16_07_56_objective.csv';
 
 % Number of values to show
 numPoints = 50;
@@ -12,13 +12,13 @@ numPoints = 50;
 %% GET DATA FROM CSV%%
 path = strcat(logsPath,csvName);
 opts = detectImportOptions(path);
-M = readmatrix(path, opts);
+T = readtable(path, opts);
 
 % Get the last numPoints rows
-M = M(max(1,(end-(numPoints - 1))):end, :);
+T = T(max(1,(end-(numPoints - 1))):end, :);
 
 % Extract timestamp and convert from epoch to datetime
-timestampEpoch = M(:,1);
+timestampEpoch = T.created_at;
 timestampDate = datetime(timestampEpoch,'ConvertFrom','posixtime','TimeZone','Europe/Zurich','Format','dd-MMM-yyyy HH:mm:ss');
 
 
@@ -29,20 +29,21 @@ tiledlayout(3,1);
 % Position
 ax1 = nexttile;
 hold on
-hplot1 = plot(timestampDate, M(:,2), '-o', 'DisplayName', 'US');
-hplot2 = plot(timestampDate, M(:,3), '-*', 'DisplayName', 'Optical');
-hplot3 = plot(timestampDate, M(:,4), '-pentagram', 'DisplayName', 'Filtered');
+hplot1 = plot(timestampDate, T.field1, '-o', 'DisplayName', 'US');
+hplot2 = plot(timestampDate, T.field2, '-*', 'DisplayName', 'Optical');
+hplot3 = plot(timestampDate, T.field3, '-pentagram', 'DisplayName', 'Filtered');
+hplot8 = plot(timestampDate, T.field8, '-.', 'DisplayName', 'Objective');
 title('Position');
-legend([hplot1, hplot2, hplot3]);
+legend([hplot1, hplot2, hplot3, hplot8]);
 grid(ax1,'on')
 hold off
 
 % Velocity
 ax2 = nexttile;
 hold on
-hplot5 = plot(timestampDate, M(:,6), '-o', 'DisplayName', 'US');
-hplot6 = plot(timestampDate, M(:,7), '-*', 'DisplayName', 'Optical');
-hplot7 = plot(timestampDate, M(:,8), '-pentagram', 'DisplayName', 'Filtered');
+hplot5 = plot(timestampDate, T.field5, '-o', 'DisplayName', 'US');
+hplot6 = plot(timestampDate, T.field6, '-*', 'DisplayName', 'Optical');
+hplot7 = plot(timestampDate, T.field7, '-pentagram', 'DisplayName', 'Filtered');
 title('Velocity');
 legend([hplot5, hplot6, hplot7]);
 grid(ax2,'on')
@@ -50,7 +51,7 @@ hold off
 
 % Rps
 ax3 = nexttile;
-hplot4 = plot(timestampDate, M(:,5), '-*', 'DisplayName', 'Optical');
+hplot4 = plot(timestampDate, T.field4, '-*', 'DisplayName', 'Optical');
 title('RPS');
 legend(hplot4);
 grid(ax3,'on')
