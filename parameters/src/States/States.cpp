19a25fa7fcba342bcc42bb53c061001eee45cc39
Lamberto Preite <78571193+Lamberto31@users.noreply.github.com>
2023-12-05 18:17:35 +0100

    Aggiunta libreria States

---
+++ parameters/src/States/States.cpp
@@ -0,0 +1,51 @@
+#include "States.h"
+#include <Arduino.h>
+
+void stateChange(State *st, byte dest) {
+  st->current = dest;
+}
+void stateNewCmd(State *st, byte command) {
+  st->command = command;
+  st->cmd_executed = false;
+}
+void stateCmdExecuted(State *st) {
+  st->cmd_executed = true;
+}
+void stateNewDirection(State *st, byte direction) {
+  st->direction = direction;
+}
+
+void printMeasures(Measures *ms) {
+  Serial.println(F("LAST MEASURES"));
+  Serial.print(F("Quantity"));
+  Serial.print(F("\tUnits"));
+  Serial.print(F("\tRaw"));
+  Serial.println(F("\tFiltered"));
+
+  Serial.print(F("Distance US\t[cm]\t"));
+  Serial.print(ms->distanceUS);
+  Serial.print(F("\t"));
+  Serial.println(ms->distanceUSFiltered);
+
+  Serial.print(F("Distance OPT\t[cm]\t"));
+  Serial.print(ms->distanceOptical);
+  Serial.print(F("\t"));
+  Serial.println(F("---"));
+
+  Serial.print(F("Rev per second\t[r/s]\t"));
+  Serial.print(ms->rpsOptical);
+  Serial.print(F("\t"));
+  Serial.println(F("---"));
+
+  Serial.print(F("Velocity US\t[cm/s]\t"));
+  Serial.print(ms->velocityUS);
+  Serial.print(F("\t"));
+  Serial.println(F("---"));
+
+  Serial.print(F("Velocity OPT\t[cm/s]\t"));
+  Serial.print(ms->velocityOptical);
+  Serial.print(F("\t"));
+  Serial.println(ms->velocityOpticalFiltered);
+
+  Serial.println();
+}
---
+++ parameters/src/States/States.h
@@ -0,0 +1,45 @@
+#ifndef _STATES_H_
+#define _STATES_H_
+
+#include <Arduino.h>
+
+#define STATE_SETUP 0
+#define STATE_FREE 1
+#define STATE_SEARCH 2
+#define STATE_MEASURE 3
+#define STATE_READ 4
+
+#define DIRECTION_STOP 0
+#define DIRECTION_FORWARD 1
+#define DIRECTION_BACKWARD 2
+#define DIRECTION_RIGHT 3
+#define DIRECTION_LEFT 4
+
+struct State {
+  byte current;
+  byte command;
+  bool cmd_executed;
+  byte direction;
+};
+
+struct Measures {
+  double distanceUS;
+  double distanceUSFiltered;
+  double velocityUS;
+
+  double distanceOptical;
+  double rpsOptical;
+  double velocityOptical;
+  double velocityOpticalFiltered;
+
+  bool sent;
+};
+
+void stateChange(State *st, byte dest);
+void stateNewCmd(State *st, byte command);
+void stateCmdExecuted(State *st);
+void stateNewDirection(State *st, byte direction);
+
+void printMeasures(Measures *ms);
+
+#endif
