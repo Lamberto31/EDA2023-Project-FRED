922047b9dac756f04e3d75f084f69fe4d91f323f
Lamberto Preite <78571193+Lamberto31@users.noreply.github.com>
2023-12-10 13:17:04 +0100

    Fix stampParams

--- raspberry/FRED_data_transmission.py
+++ raspberry/FRED_data_transmission.py
@@ -308,8 +308,8 @@ if PARAMS:
         if ((DEBUG == "Default" or DEBUG == "Full") and VIEW_DATA):
             print("\nPARAMS")
             print("N.\tattempt\tcurrentTime\tdistance\tspeed\tstopTime\tstatus\n")
-            for i in range(0, len(dataToSend)):
-                print(str(i + 1) + "\t" + str(dataToSend[i]["attempt"]) + "\t" + str(dataToSend[i]["currentTime"]) + "\t" + str(dataToSend[i]["distance"]) + "\t" + str(dataToSend[i]["speed"]) + "\t" + str(dataToSend[i]["stopTime"]) + "\t" + str(dataToSend[i]["status"]) + "\n")
+            for i in range(0, len(params)):
+                print(str(i + 1) + "\t" + str(params[i]["attempt"]) + "\t" + str(params[i]["currentTime"]) + "\t" + str(params[i]["distance"]) + "\t" + str(params[i]["speed"]) + "\t" + str(params[i]["stopTime"]) + "\t" + str(params[i]["status"]) + "\n")
 
 
 # MAIN LOOP: receive data from Bluetooth and send to remote server via WiFi
