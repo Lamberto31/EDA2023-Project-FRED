d1f9445b702b2386840dd59d56a6371ef29d3ecd
Lamberto Preite <78571193+Lamberto31@users.noreply.github.com>
2023-12-14 13:07:28 +0100

    Commentato invio gain

--- EDA2023-Project-FRED.ino
+++ EDA2023-Project-FRED.ino
@@ -893,12 +893,15 @@ void bluetoothSendFilterResult() {
   // Velocity_covariance
   Serial.println(P_hat(1, 1), DECIMALS);
 
+  // TODO: Capire se serve
+  /*
   Serial.print(F("Gain:"));
   // TODO: Capire se va bene così o tutta la matrice (STATE_DIM x MEASURE_DIM)
   // Position_gain
   Serial.print(W(0, 0), DECIMALS);
   // Velocity_gain
   Serial.println(W(1, 0), DECIMALS);
+  */
 
   Serial.println(F("BDT 1.0 END"));
 }
