e8ed1be74035ac94ad4bdacc8d5abb105757d95f
Lamberto Preite <78571193+Lamberto31@users.noreply.github.com>
2023-12-30 17:59:41 +0100

    Matlab: adattato view_data_remote a nuove modifiche

--- matlab/view_data_remote.m
+++ matlab/view_data_remote.m
@@ -14,68 +14,88 @@ readAPIKey = string(T.readapiky);
 
 
 %% FIELDS %%
+% Input
+inputId = 1;
+
 % Position
-positionUSId = 1;
-positionOpticalId = 2;
-positionFilteredId = 3;
-positionObjectiveId = 8;
+positionMeasureId = 2;
+positionEstimateId = 4;
+positionCovarianceId = 6;
 
 % Velocity
-velocityUSId = 5;
-velocityOpticalId = 6;
-velocityFilteredId = 7;
-
-% RPS
-rpsId = 4;
+velocityMeasureId = 3;
+velocityEstimateId = 5;
+velocityCovarianceId = 7;
 
 
 %% GET DATA FROM THINGSPEAK CHANNEL%%
+% Input
+[input, time1] = thingSpeakRead(channelID, 'Field', inputId, 'NumPoints', numPoints, 'ReadKey', readAPIKey);
+
 % Position
-[positionUS, time1] = thingSpeakRead(channelID, 'Field', positionUSId, 'NumPoints', numPoints, 'ReadKey', readAPIKey);
-[positionOptical, time2] = thingSpeakRead(channelID, 'Field', positionOpticalId, 'NumPoints', numPoints, 'ReadKey', readAPIKey);
-[positionFiltered, time3] = thingSpeakRead(channelID, 'Field', positionFilteredId, 'NumPoints', numPoints, 'ReadKey', readAPIKey);
-[positionObjective, time8] = thingSpeakRead(channelID, 'Field', positionObjectiveId, 'NumPoints', numPoints, 'ReadKey', readAPIKey);
+[positionMeasure, time2] = thingSpeakRead(channelID, 'Field', positionMeasureId, 'NumPoints', numPoints, 'ReadKey', readAPIKey);
+[positionEstimate, time4] = thingSpeakRead(channelID, 'Field', positionEstimateId, 'NumPoints', numPoints, 'ReadKey', readAPIKey);
+[positionCovariance, time6] = thingSpeakRead(channelID, 'Field', positionCovarianceId, 'NumPoints', numPoints, 'ReadKey', readAPIKey);
 
 % Velocity
-[velocityUS, time5] = thingSpeakRead(channelID, 'Field', velocityUSId, 'NumPoints', numPoints, 'ReadKey', readAPIKey);
-[velocityOptical, time6] = thingSpeakRead(channelID, 'Field', velocityOpticalId, 'NumPoints', numPoints, 'ReadKey', readAPIKey);
-[velocityFiltered, time7] = thingSpeakRead(channelID, 'Field', velocityFilteredId, 'NumPoints', numPoints, 'ReadKey', readAPIKey);
-
-% RPS
-[rps, time4] = thingSpeakRead(channelID, 'Field', rpsId, 'NumPoints', numPoints, 'ReadKey', readAPIKey);
+[velocityMeasure, time3] = thingSpeakRead(channelID, 'Field', velocityMeasureId, 'NumPoints', numPoints, 'ReadKey', readAPIKey);
+[velocityEstimate, time5] = thingSpeakRead(channelID, 'Field', velocityEstimateId, 'NumPoints', numPoints, 'ReadKey', readAPIKey);
+[velocityCovariance, time7] = thingSpeakRead(channelID, 'Field', velocityCovarianceId, 'NumPoints', numPoints, 'ReadKey', readAPIKey);
 
 
 %% VISUALIZE DATA %%
+
+% Input
+figure();
+hold on;
+plot(time1, input, '-', 'Color', [0.9290 0.6940 0.1250]);
+title('Input');
+xlabel('Time');
+ylabel('Input');
+grid on
+
+% State (measured and estimated)
+figure();
 % Layout definition
-tiledlayout(3,1);
+tiledlayout(2,1);
 
 % Position
 ax1 = nexttile;
 hold on
-hplot1 = plot(time1, positionUS, '-o', 'DisplayName', 'US');
-hplot2 = plot(time2, positionOptical, '-*', 'DisplayName', 'Optical');
-hplot3 = plot(time3, positionFiltered, '-pentagram', 'DisplayName', 'Filtered');
-hplot8 = plot(time8, positionObjective, '-.', 'DisplayName', 'Objective');
+hplot1 = plot(time2, positionMeasure, '-o', 'DisplayName', 'Ultrasonic distance', 'Color', [0.4940 0.1840 0.5560]);
+hplot2 = plot(time4, positionEstimate, '-*', 'DisplayName', 'Position estimate', 'Color', [0.4660 0.6740 0.1880]);
 title('Position');
-legend([hplot1, hplot2, hplot3, hplot8]);
+legend([hplot1, hplot2]);
 grid(ax1,'on')
 hold off
 
 % Velocity
 ax2 = nexttile;
 hold on
-hplot5 = plot(time5, velocityUS, '-o', 'DisplayName', 'US');
-hplot6 = plot(time6, velocityOptical, '-*', 'DisplayName', 'Optical');
-hplot7 = plot(time7, velocityFiltered, '-pentagram', 'DisplayName', 'Filtered');
+hplot3 = plot(time3, velocityMeasure, '-o', 'DisplayName', 'Optical pulses', 'Color', [0.4940 0.1840 0.5560]);
+hplot4 = plot(time5, velocityEstimate, '-*', 'DisplayName', 'Velocity estimate', 'Color', [0.4660 0.6740 0.1880]);
 title('Velocity');
-legend([hplot5, hplot6, hplot7]);
+legend([hplot3, hplot4]);
 grid(ax2,'on')
 hold off
 
-% Rps
-ax3 = nexttile;
-hplot4 = plot(time4, rps, '-*', 'DisplayName', 'Optical');
-title('RPS');
-legend(hplot4);
-grid(ax3,'on')
+% State Covariance
+figure();
+% Layout definition
+tiledlayout(2,1);
 
+% Position
+ax3 = nexttile;
+hold on
+hplot5 = plot(time6, positionCovariance, '-', 'DisplayName', 'Position covariance');
+title('Position Covariance');
+grid(ax3,'on')
+hold off
+
+% Velocity
+ax4 = nexttile;
+hold on
+hplot6 = plot(time7, velocityCovariance, '-', 'DisplayName', 'Velocity covariance');
+title('Velocity Covariance');
+grid(ax4,'on')
+hold off
